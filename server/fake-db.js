ea10d9ac74ad86aad3b3a538619a15936179e4ef
kenshiozaki <salty815@gmail.com>
2023-06-18 14:29:59 +0900

    make node server

---
+++ server/fake-db.js
@@ -0,0 +1,81 @@
+const Product = require('./model/product')
+
+class FakeDb {
+
+    constructor() {
+        this.products = [
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone XL',
+                price: 799,
+                description: 'A large phone with one of the best screens',
+                heading1: 'sample text1',
+                heading2: 'sample text2',
+                heading3: 'sample text3',
+                headingtext1: 'sample message1',
+                headingtext2: 'sample message2',
+                headingtext3: 'sample message3',
+            },
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Mini',
+                price: 699,
+                description: 'A great phone with one of the best cameras',
+                heading1: 'sample text1',
+                heading2: 'sample text2',
+                heading3: 'sample text3',
+                headingtext1: 'sample message1',
+                headingtext2: 'sample message2',
+                headingtext3: 'sample message3',
+            },
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Standard',
+                price: 299,
+                description: '',
+                heading1: 'sample text1',
+                heading2: 'sample text2',
+                heading3: 'sample text3',
+                headingtext1: 'sample message1',
+                headingtext2: 'sample message2',
+                headingtext3: 'sample message3',
+            },
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Special',
+                price: 99,
+                description: '',
+                heading1: 'sample text1',
+                heading2: 'sample text2',
+                heading3: 'sample text3',
+                headingtext1: 'sample message1',
+                headingtext2: 'sample message2',
+                headingtext3: 'sample message3',
+            }
+        ]
+    }
+
+    async initDb() {
+        await this.cleanDb()
+        this.pushProductsTodo()
+    }
+
+    async cleanDb() {
+        await Product.deleteMany({})
+    }
+
+    pushProductsTodo() {
+        this.products.forEach(
+            (product) => {
+                const newProduct = new Product(product)
+                newProduct.save()
+            }
+        )
+    }
+
+    seeDb() {
+        this.pushProductsTodo()
+    }
+}
+
+module.exports = FakeDb
